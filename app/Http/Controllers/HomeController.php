8eae9322cf811aebf32b253e95f1698a3ceba90b
Fadhiel Ibnu Sina <fadhielsina@gmail.com>
2024-06-07 02:51:18 +0700

    checkin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -55,21 +55,8 @@ class HomeController extends Controller
         return view('form_checkin');
     }
 
-    public function checkin_submit()
+    public function checkin_submit(Request $request)
     {
-        $img = $_POST['image'];
-        $folderPath = "upload/";
-
-        $image_parts = explode(";base64,", $img);
-        $image_type_aux = explode("image/", $image_parts[0]);
-        $image_type = $image_type_aux[1];
-
-        $image_base64 = base64_decode($image_parts[1]);
-        $fileName = uniqid() . '.png';
-
-        $file = $folderPath . $fileName;
-        file_put_contents($file, $image_base64);
-
-        print_r($fileName);
+        dd($request);
     }
 }
